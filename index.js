9444958cf67445b1e8ae93a93d51c6b067850d0a
Fatimah Sarwar <fatimahsarwar@Fatimahs-MBP.fios-router.home>
2022-03-10 12:25:38 -0500

    finished making files

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const server = express();
+
+
+server.get('/', (req,res)=>{
+    res.end('<h1>Hello World</h1>')
+})
+
+
+
+
+
+server.listen(9000,()=>{
+    console.log('server running on 9000')
+})
+
